e8100aa8b1ff532cfa98e0e8ea27829f2dec9266
XAldrikX <nicolasbortoli2010@hotmail.com>
2021-12-28 04:08:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,125 @@
+let player1Score = 0
+let player2Score = 0
+let player1Turn = null
+
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+const whoBtn = document.getElementById("whoStarts")
+const dblOrNothing = document.getElementById("dblOrNothing")
+
+function showResetButton() {
+    rollBtn.style.display = "none"
+    dblOrNothing.style.display = "none"
+    resetBtn.style.display = "block"
+}
+
+dblOrNothing.addEventListener("click", function() {
+    let randomNumber_1or2 = Math.floor(Math.random() * 2) + 1
+    let randomNumber = Math.floor(Math.random() * 6) + 1
+    
+    if (randomNumber_1or2 === 1) {
+        randomNumber = 0
+    } else {
+        randomNumber = randomNumber * 2
+    }
+
+    if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+    } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }
+    
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 Won 🥳"
+        showResetButton()
+    }  else if (player2Score >= 20) {
+        message.textContent = "Player 2 Won 🎉"
+        showResetButton()
+    }
+    player1Turn = !player1Turn
+})
+
+whoBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 2) + 1
+    
+    if (randomNumber === 1) {
+        message.style.display = "block"
+        message.textContent = "Player 1 Turn"
+        player1Turn = true
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+    } else {
+        message.style.display = "block"
+        message.textContent = "Player 2 Turn"
+        player1Turn = false
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+    }
+    
+    whoBtn.style.display = "none"
+    rollBtn.style.display = "block"
+    dblOrNothing.style.display = "block"
+    
+})
+
+ rollBtn.addEventListener("click", function() {
+    let randomNumber = Math.floor(Math.random() * 6) + 1
+
+    if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+    } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }
+    
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 Won 🥳"
+        showResetButton()
+    }  else if (player2Score >= 20) {
+        message.textContent = "Player 2 Won 🎉"
+        showResetButton()
+    }
+    player1Turn = !player1Turn
+})
+ 
+resetBtn.addEventListener("click", function(){
+    reset()
+})
+
+function reset() {
+    player1Score = 0
+    player2Score = 0
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    resetBtn.style.display = "none"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+    whoBtn.style.display = "block"
+    message.style.display = "none"
+}
